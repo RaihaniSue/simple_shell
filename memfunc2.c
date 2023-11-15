50b36d4f101b78404bfec454ae6c0bcb1c13f199
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 19:37:57 +0100

    The bfree function frees a pointer and sets its address to NULL, returning 1 if the pointer was freed and 0 otherwise.

--- memfunc2.c
+++ memfunc2.c
@@ -1,10 +1,8 @@
 #include "shell.h"
-
 /**
- * bfree - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
- *
- * Return: 1 if freed, otherwise 0.
+ * bfree - entry point
+ * @ptr: ptr
+ * Return: 1 on success 0 failure
  */
 int bfree(void **ptr)
 {

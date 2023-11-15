6defec22d4cb6226803d416487e624ddca152a1f
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 19:09:07 +0100

    The program defines functions related to command chaining, alias replacement, and variable substitution in a shell. The is_chain function checks if the current character in the buffer is a chaining delimiter, updating the command buffer type accordingly. The check_chain function determines whether to continue chaining based on the last command's status. The replace_alias function replaces aliases in the tokenized string. The replace_vars function replaces variables in the tokenized string, including special variables like $? and $$. The replace_string function replaces a string with a new one.

--- func1.c
+++ func1.c
@@ -1,48 +1,46 @@
 #include "shell.h"
 
 /**
- * is_chain - test if current char in buffer is a chain delimeter
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- *
- * Return: 1 if chain delimeter, 0 otherwise
+ * is_chain - entry point
+ * @info: data struct
+ * @buf: buffer in char
+ * @p: ptr on buff
+ * Return: 1 on delimeter and 0 otherwise
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
-	size_t j = *p;
+	size_t i = *p;
 
-	if (buf[j] == '|' && buf[j + 1] == '|')
+	if (buf[i] == '|' && buf[i + 1] == '|')
 	{
-		buf[j] = 0;
-		j++;
+		buf[i] = 0;
+		i++;
 		info->cmd_buf_type = CMD_OR;
 	}
-	else if (buf[j] == '&' && buf[j + 1] == '&')
+	else if (buf[i] == '&' && buf[i + 1] == '&')
 	{
-		buf[j] = 0;
-		j++;
+		buf[i] = 0;
+		i++;
 		info->cmd_buf_type = CMD_AND;
 	}
-	else if (buf[j] == ';') /* found end of this command */
+	else if (buf[i] == ';')
 	{
-		buf[j] = 0; /* replace semicolon with null */
+		buf[i] = 0;
 		info->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
-	*p = j;
+	*p = i;
 	return (1);
 }
 
 /**
- * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
- *
+ * check_chain - second entry point
+ * @info: data struct
+ * @buf: buffer to char
+ * @p: ptr to current
+ * @i: the pos start in buff
+ * @len: buff size
  * Return: Void
  */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
@@ -70,18 +68,17 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 }
 
 /**
- * replace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
+ * replace_alias - third entry point
+ * @info: data struct
+ * Return: 1 in replacement and 0 otherwise
  */
 int replace_alias(info_t *info)
 {
-	int i;
+	int k;
 	list_t *node;
 	char *p;
 
-	for (i = 0; i < 10; i++)
+	for (k = 0; k < 10; k++)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
 		if (!node)
@@ -99,52 +96,50 @@ int replace_alias(info_t *info)
 }
 
 /**
- * replace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
+ * replace_vars - fourth entry point
+ * @info: data struct
+ * Return: 1 always on success
  */
 int replace_vars(info_t *info)
 {
-	int i = 0;
+	int j = 0;
 	list_t *node;
 
-	for (i = 0; info->argv[i]; i++)
+	for (j = 0; info->argv[j]; j++)
 	{
-		if (info->argv[i][0] != '$' || !info->argv[i][1])
+		if (info->argv[j][0] != '$' || !info->argv[j][1])
 			continue;
 
-		if (!_strcmp(info->argv[i], "$?"))
+		if (!_strcmp(info->argv[j], "$?"))
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[j]),
 				_strdup(convert_number(info->status, 10, 0)));
 			continue;
 		}
-		if (!_strcmp(info->argv[i], "$$"))
+		if (!_strcmp(info->argv[j], "$$"))
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[j]),
 				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_starts_with(info->env, &info->argv[i][1], '=');
+		node = node_starts_with(info->env, &info->argv[j][1], '=');
 		if (node)
 		{
-			replace_string(&(info->argv[i]),
+			replace_string(&(info->argv[j]),
 				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
-		replace_string(&info->argv[i], _strdup(""));
+		replace_string(&info->argv[j], _strdup(""));
 
 	}
 	return (0);
 }
 
 /**
- * replace_string - replaces string
- * @old: address of old string
- * @new: new string
- *
- * Return: 1 if replaced, 0 otherwise
+ * replace_string - fifth entry point
+ * @old: add to old string
+ * @new: str
+ * Return: 1 always on success others otherwise
  */
 int replace_string(char **old, char *new)
 {

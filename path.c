1555572d960170e11b1ef50362f6a66ef80ed6c4
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 19:48:03 +0100

    The is_cmd function determines if a file is an executable command, the dup_chars function duplicates characters from a specified range in a string, and the find_path function searches for a command in the PATH string, returning the full path if found or NULL otherwise.

--- path.c
+++ path.c
@@ -1,11 +1,9 @@
 #include "shell.h"
-
 /**
- * is_cmd - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
- *
- * Return: 1 if true, 0 otherwise
+ * is_cmd - entry point
+ * @info: data struct
+ * @path: file ath
+ * Return: 0 on success others otherwise
  */
 int is_cmd(info_t *info, char *path)
 {
@@ -23,12 +21,11 @@ int is_cmd(info_t *info, char *path)
 }
 
 /**
- * dup_chars - duplicates characters
- * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
+ * dup_chars - second entry point
+ * @pathstr: string path
+ * @start: index starrt
+ * @stop: index stop
+ * Return: ptr
  */
 char *dup_chars(char *pathstr, int start, int stop)
 {
@@ -41,14 +38,12 @@ char *dup_chars(char *pathstr, int start, int stop)
 	buf[k] = 0;
 	return (buf);
 }
-
 /**
- * find_path - finds this cmd in the PATH string
- * @info: the info struct
- * @pathstr: the PATH string
- * @cmd: the cmd to find
- *
- * Return: full path of cmd if found or NULL
+ * find_path - third entry point
+ * @info: data struct
+ * @pathstr: str path
+ * @cmd: the cmd to be found
+ * Return: the oath or null
  */
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {

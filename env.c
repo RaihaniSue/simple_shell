ddf959d90970b818c42b0804b1798739f48717e5
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 18:59:34 +0100

    The program defines functions for handling environment variables in a shell, allowing users to print the current environment, retrieve the value of a specific environment variable, set or modify an environment variable, unset one or more environment variables, and populate the environment variable linked list from the system environment.

--- env.c
+++ env.c
@@ -1,9 +1,8 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myenv - entry point
+ * @info: data struct
  * Return: Always 0
  */
 int _myenv(info_t *info)
@@ -13,11 +12,10 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
+ * _getenv - second entry point
+ * @info: data struct
+ * @name: envi name
+ * Return: NULL val
  */
 char *_getenv(info_t *info, const char *name)
 {
@@ -35,11 +33,9 @@ char *_getenv(info_t *info, const char *name)
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _mysetenv - third entry point
+ * @info: data struct
+ * Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
@@ -54,39 +50,37 @@ int _mysetenv(info_t *info)
 }
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myunsetenv - fourth entry point
+ * @info: data struct
+ * Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
-	int i;
+	int j;
 
 	if (info->argc == 1)
 	{
 		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+	for (j = 1; j <= info->argc; j++)
+		_unsetenv(info, info->argv[j]);
 
 	return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * populate_env_list - fifth entry point
+ * @info: data struct
  * Return: Always 0
  */
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
-	size_t i;
+	size_t j;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
+	for (j = 0; environ[j]; j++)
+		add_node_end(&node, environ[j], 0);
 	info->env = node;
 	return (0);
 }

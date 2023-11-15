c9994d2e86fc8ba6ceda6a9be337d4262528c815
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 19:17:30 +0100

    The code defines the main shell loop (hsh) and related functions for finding and executing built-in commands (find_builtin), external commands (find_cmd), and forking a child process to run a command (fork_cmd). The shell supports various built-in commands, and it handles command chaining, input reading, and signals.

--- hsh.c
+++ hsh.c
@@ -1,11 +1,9 @@
 #include "shell.h"
-
 /**
- * hsh - main shell loop
- * @info: the parameter & return info struct
- * @av: the argument vector from main()
- *
- * Return: 0 on success, 1 on error, or error code
+ * hsh - entry point
+ * @info: data struct
+ * @av: the vec arg
+ * Return: 0 always on success others otherwise
  */
 int hsh(info_t *info, char **av)
 {
@@ -44,13 +42,9 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- *			0 if builtin executed successfully,
- *			1 if builtin found but not successful,
- *			-2 if builtin signals exit()
+ * find_builtin - second entry point
+ * @info: data struct
+ * Return: always 0 on success others otherwise
  */
 int find_builtin(info_t *info)
 {
@@ -78,9 +72,8 @@ int find_builtin(info_t *info)
 }
 
 /**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
- *
+ * find_cmd - third entry point
+ * @info: data struct
  * Return: void
  */
 void find_cmd(info_t *info)
@@ -120,9 +113,8 @@ void find_cmd(info_t *info)
 }
 
 /**
- * fork_cmd - forks a an exec thread to run cmd
- * @info: the parameter & return info struct
- *
+ * fork_cmd - fourth entry point
+ * @info: data struct
  * Return: void
  */
 void fork_cmd(info_t *info)
@@ -132,7 +124,6 @@ void fork_cmd(info_t *info)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		/* TODO: PUT ERROR FUNCTION */
 		perror("Error:");
 		return;
 	}
@@ -145,7 +136,6 @@ void fork_cmd(info_t *info)
 				exit(126);
 			exit(1);
 		}
-		/* TODO: PUT ERROR FUNCTION */
 	}
 	else
 	{

10e7dc2146204dbcef7dd2b8c1eef8eb9e6fd1ed
RaihaniSue <142739525+RaihaniSue@users.noreply.github.com>
2023-11-15 19:36:01 +0100

    The _memset function fills a memory area with a constant byte, the ffree function frees a dynamically allocated array of strings, and the _realloc function reallocates a block of memory with a new size, handling cases where the pointer is NULL, the new size is zero, or the new size is equal to the old size.

--- memfunc.c
+++ memfunc.c
@@ -1,11 +1,10 @@
 #include "shell.h"
-
 /**
- **_memset - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
+ **_memset - first entry point
+ *@s: ptr to memory
+ *@b: to fill
+ *@n: how much to fill
+ *Return: ptr to memory
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -15,10 +14,9 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 	return (s);
 }
-
 /**
- * ffree - frees a string of strings
- * @pp: string of strings
+ * ffree - second entry point
+ * @pp: strings
  */
 void ffree(char **pp)
 {
@@ -32,12 +30,11 @@ void ffree(char **pp)
 }
 
 /**
- * _realloc - reallocates a block of memory
- * @ptr: pointer to previous malloc'ated block
- * @old_size: byte size of previous block
- * @new_size: byte size of new block
- *
- * Return: pointer to da ol'block nameen.
+ * _realloc - third entry point
+ * @ptr: ptr
+ * @old_size: size 
+ * @new_size: size of new
+ * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
